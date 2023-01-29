6bece3a9236bb6df7bf4322ee0e1e518b4be13ed
Koras02 <wjdgh0727@gmail.com>
2023-01-30 00:34:06 +0900

    electron app

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
